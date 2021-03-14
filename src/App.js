48915e59e3b8798fa029d75171293492c4238ff1
Carolina R <carolinarizk@gmail.com>
2021-03-14 12:24:37 -0400

    practiced hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import ViewCount from "./components/ViewCount";
 
+//hooks must b defined on top and not used in conditionals (if, switch...etc)
+//useMemo
 function App() {
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    console.log(counter);
+  }, [counter]);
+
   return (
     <div className="App">
       <h1>My React Portfolio</h1>
+      <ViewCount value="my number" sample={12} counter={counter} />
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        click
+      </button>
     </div>
   );
 }
---
+++ src/components/ViewCount.js
@@ -0,0 +1,9 @@
+export default function ViewCount({ value, sample, counter }) {
+  return(
+    <div>
+    <h1>{value}</h1>
+    <h2>{sample}</h2>
+    <h2>{counter}</h2>
+    </div>
+  )
+}
