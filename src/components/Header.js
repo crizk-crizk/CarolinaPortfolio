9a7575898d792c614b3a348a1c0ce2a55152af27
Carolina R <carolinarizk@gmail.com>
2021-03-19 16:31:34 -0400

    added footer & header

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <Navbar collapseOnSelect bg="light" expand="lg">
+      <Navbar.Brand href="#about">Carolina</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="#about">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="nav-link" href="#mywork">
+              My Work
+            </a>
+          </li>
+          <a className="nav-link" href="#contact">
+            Contact me
+          </a>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
