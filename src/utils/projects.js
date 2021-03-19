5b4ab8a4044794d85eac8111e7b93a2e23c7aecf
Carolina R <carolinarizk@gmail.com>
2021-03-19 16:33:19 -0400

    added projects data

---
+++ src/utils/projects.js
@@ -0,0 +1,19 @@
+const projects = [
+  {
+    title: "Weather Dashboard",
+    deployed: "",
+    github: "",
+    tech: ["JavaScript", "CSS5", "HTML5"],
+    image: "/images/face.jpeg", 
+    //host images in amazon webservices AWS S3 image buckets. put link here. 
+  },
+  {
+    title: "Wine Log",
+    deployed: "",
+    github: "",
+    tech: ["JavaScript", "CSS5", "HTML5"],
+    image: "",
+  }
+];
+
+export default projects;
